fa1aec63976381bd486fb5039eaa695a18ee1e86
hakbrk <jhjhawkins@gmail.com>
2020-05-01 17:10:37 -0500

    please work

--- app.py
+++ app.py
@@ -7,8 +7,8 @@
 
 # import flask
 import dash
-from dash.dependencies import Input, Output
-import dash_core_components as dcc
+# from dash.dependencies import Input, Output
+# import dash_core_components as dcc
 import dash_html_components as html
 external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 
