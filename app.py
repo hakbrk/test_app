4bf5271f6750bdda28845726542a0a7e60bb245f
hakbrk <jhjhawkins@gmail.com>
2020-05-01 16:58:42 -0500

    updated

--- app.py
+++ app.py
@@ -16,7 +16,7 @@ import dash_html_components as html
 # Make sure not to change this file name or the variable names below,
 # the template is configured to execute 'server' on 'app.py'
 server = flask.Flask(__name__)
-server.secret_key = os.environ.get('secret_key', str(randint(0, 1000000)))
+# server.secret_key = os.environ.get('secret_key', str(randint(0, 1000000)))
 app = dash.Dash(__name__, server=server)
 
 
