a66cd8c6ccfc97bcac7933978f8d6f63f30bac46
hakbrk <jhjhawkins@gmail.com>
2020-05-18 13:05:41 -0500

    Added chromedriver stuff

--- db_update.py
+++ db_update.py
@@ -7,11 +7,16 @@ from tqdm import tqdm_notebook
 from selenium import webdriver
 from pandas.io.html import read_html
 import time
-from selenium.webdriver.chrome.options import Options
+from selenium import webdriver
+import os
 
 engine = create_engine('mysql+mysqlconnector://b299c42f0fdf61:fcdc6acd@us-cdbr-east-06.cleardb.net/heroku_826bb11c8d537f8')
-chrome_options = Options()
-chrome_options.add_argument('--headless')
+chrome_options = webdriver.ChromeOptions()
+chrome_options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
+chrome_options.add_argument("--headless")
+chrome_options.add_argument("--disable-dev-shm-usage")
+chrome_options.add_argument("--no-sandbox")
+driver = webdriver.Chrome(executable_path=os.environ.get("CHROMEDRIVER_PATH"), chrome_options=chrome_options)
 
 query = """SELECT DISTINCT ticker FROM equity_history"""
 answer = pd.read_sql_query(query, engine)
