7fe230093a3e2592bfecc52c4969e397aaa826d6
hakbrk <jhjhawkins@gmail.com>
2020-05-28 16:42:27 -0500

    update db_update code

--- db_update.py
+++ db_update.py
@@ -102,10 +102,10 @@ from webdriver_manager.chrome import ChromeDriverManager
 
 engine = create_engine('mysql+mysqlconnector://b299c42f0fdf61:fcdc6acd@us-cdbr-east-06.cleardb.net/heroku_826bb11c8d537f8')
 chrome_options = Options()
-chrome_options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
+# chrome_options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
 chrome_options.add_argument("--headless")
-chrome_options.add_argument("--disable-dev-shm-usage")
-chrome_options.add_argument("--no-sandbox")
+# chrome_options.add_argument("--disable-dev-shm-usage")
+# chrome_options.add_argument("--no-sandbox")
 
 
 query = """SELECT DISTINCT ticker FROM equity_history"""
