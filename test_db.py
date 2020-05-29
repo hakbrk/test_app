2bd8aee0b11f5a0c9195cf57efbbd0cd032b9a73
hakbrk <jhjhawkins@gmail.com>
2020-05-29 12:00:03 -0500

    update

--- test_db.py
+++ test_db.py
@@ -2,17 +2,17 @@ from selenium import webdriver
 import os
 
 chrome_options = webdriver.ChromeOptions()
-# chrome_options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
+chrome_options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
 chrome_options.add_argument("--headless")
 chrome_options.add_argument("--disable-dev-shm-usage")
 chrome_options.add_argument("--no-sandbox")
-# driver = webdriver.Chrome(executable_path=os.environ.get("CHROMEDRIVER_PATH"), chrome_options=chrome_options)
+driver = webdriver.Chrome(executable_path=os.environ.get("CHROMEDRIVER_PATH"), chrome_options=chrome_options)
 
-# print(driver.page_source)
+print(driver.page_source)
 
-chrome_bin = os.environ.get('GOOGLE_CHROME_SHIM', None)
-# opts = ChromeOptions()
-chrome_options.binary_location = chrome_bin
-browser = webdriver.Chrome(executable_path="chromedriver", chrome_options=chrome_options)
+# chrome_bin = os.environ.get('GOOGLE_CHROME_SHIM', None)
+# # opts = ChromeOptions()
+# chrome_options.binary_location = chrome_bin
+# browser = webdriver.Chrome(executable_path="chromedriver", chrome_options=chrome_options)
 
-print(browser.page_source)
+# print(browser.page_source)
